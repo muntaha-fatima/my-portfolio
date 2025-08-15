881bbaaf63494a9c1a823e95b561239fedea2847
muntaha-fatima <shazsabir6@gmail.com>
2025-08-15 17:41:04 +0500

    portfolio push to github

--- script.js
+++ script.js
@@ -102,3 +102,9 @@ document.addEventListener('DOMContentLoaded', () => {
       form.reset();
     }
   });
+ const menuBtn = document.getElementById('menu-btn');
+  const navList = document.getElementById('nav-list');
+
+  menuBtn.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
